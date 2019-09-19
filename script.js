40df3ab4c335da53a0acbacd33aedea57c1e4935
Sain <chyh1900@gmail.com>
2019-09-19 16:23:12 +0900

    중간 업로드

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-var bodyText = document.querySelector('body').innerText;
-var bodyNum = bodyText.split(' ').length;
+function core() {
+    var filt = document.querySelector('#user').value;
+
+    chrome.storage.sync.set({
+        userWords: filt
+    });
+
+    chrome.tabs.executeScript({
+        code: `
+        document.querySelector('body').innerText;
+        `
+    }, function(result) {
+        var bodyText = result[0];
+        var bodyNum = bodyText.split(' ').length;
+        var myNum = bodyText.match(new RegExp(`\\b(${filt})\\b`, 'gi')).length;
+    
+        var per = myNum / bodyNum * 100;
+        per = per.toFixed(2);
+    
+        document.querySelector('#result').innerText = `${myNum} / ${bodyNum} (${per}%)`;
+    
+    });
+}
+
+chrome.storage.sync.get(function(data) {
+    document.querySelector('#user').value = data.userWords;
+    core();
+});
+
+document.querySelector('#user').addEventListener('change', function() {
+    var filt = document.querySelector('#user').value;
+    core();
+});
